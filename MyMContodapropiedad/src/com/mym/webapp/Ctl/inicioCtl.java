dba6ef060c8097a2d61fbd59bcb0541a711fd247
DanielGomez1 <danielgomezaristizabal@gmail.com>
2015-10-27 05:51:09 -0500

    menu

---
+++ MyMContodapropiedad/src/com/mym/webapp/Ctl/inicioCtl.java
@@ -0,0 +1,15 @@
+package com.mym.webapp.Ctl;
+
+import org.zkoss.zk.ui.util.GenericForwardComposer;
+import org.zkoss.zul.Listitem;
+import org.zkoss.zul.ListitemRenderer;
+
+public class inicioCtl extends GenericForwardComposer implements ListitemRenderer {
+
+	public void render(Listitem item, Object data, int index) throws Exception {
+		// TODO Auto-generated method stub
+		
+		
+	}
+
+}
