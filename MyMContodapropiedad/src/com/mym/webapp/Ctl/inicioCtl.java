0b1bc313b13189cf1f4b5774784cd3effc476c7d
PatricioMedina <patriciomm8@hotmail.com>
2015-11-26 21:03:40 -0500

    Controlador

--- MyMContodapropiedad/src/com/mym/webapp/Ctl/inicioCtl.java
+++ MyMContodapropiedad/src/com/mym/webapp/Ctl/inicioCtl.java
@@ -1,15 +1,25 @@
+
+
 package com.mym.webapp.Ctl;
 
+import java.util.List;
+
 import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.event.Event;
 import org.zkoss.zk.ui.util.GenericForwardComposer;
+import org.zkoss.zul.Button;
+import org.zkoss.zul.Label;
+import org.zkoss.zul.ListModel;
+import org.zkoss.zul.ListModelList;
 import org.zkoss.zul.Listitem;
 import org.zkoss.zul.ListitemRenderer;
+import org.zkoss.zul.Radiogroup;
 
-import com.mym.webapp.Dao.Propiedades;
+import com.mym.webapp.Dto.inicioDto;
 import com.mym.webapp.ngc.inicioNgc;
 
 /**
- * Clase controladora de la vista estudiante.zul  <-----
+ * Clase controladora de las vistas
  * 
  * @author DanielGomez
  *
@@ -17,15 +27,37 @@ import com.mym.webapp.ngc.inicioNgc;
 
 public class inicioCtl extends GenericForwardComposer implements ListitemRenderer {
 
+	private Button btn1;
+	private Button BtnBuscar; 
+ 	private Radiogroup RadProp;
+ 	private Radiogroup RadTipo;
+ 	
+ 	private inicioNgc InicioNgc;
+ 	
+ 	public inicioCtl(){
+		InicioNgc = inicioNgc.getIntance();
+	}
+ 	
+ 	public void onCreate(){
+ 		definirModelo();
+ 	}
+ 	
+	private void definirModelo() {
+		List<inicioDto> listaPropiedades = InicioNgc.listarPropiedades();
+		ListModel<inicioDto> model = new ListModelList<inicioDto>(listaPropiedades);		
+	}
 	
-	// -----------------------------------------------------------------
-	// Elementos de la mapeados de la vista
-	// -----------------------------------------------------------------
-	
-	
+	public void onClick$BtnBuscar(Event e){
+		System.out.println(RadProp.getSelectedItem());
+	}
+
 	public void render(Listitem item, Object data, int index) throws Exception {
 		// TODO Auto-generated method stub
 
 	}
-
+	
+	public void doAfterCompose(Component comp) throws Exception{		
+		super.doAfterCompose(comp);	
+	}
+	
 }
--- MyMContodapropiedad/src/com/mym/webapp/ngc/inicioNgc.java
+++ MyMContodapropiedad/src/com/mym/webapp/ngc/inicioNgc.java
@@ -1,7 +1,37 @@
 package com.mym.webapp.ngc;
 
+import java.util.List;
+
+import com.mym.webapp.Dao.inicioDao;
+import com.mym.webapp.Dto.inicioDto;
+import com.mym.webapp.util.ExcepcionWeb;
 
 
-public class inicioNgc {
+
+public class inicioNgc{
+	
+	private inicioDao InicioDao;
+	
+	private static inicioNgc InicioNgc;
+	
+	private inicioNgc(){
+		InicioDao = new inicioDao();
+	}
+	
+	public static inicioNgc getIntance(){
+		if(InicioNgc == null){
+			InicioNgc = new inicioNgc();
+		}
+		return InicioNgc;
+	}
+
+	public void crearPropiedad(inicioDto InicioDto) throws ExcepcionWeb {
+		InicioDao.buscarPropiedad(InicioDto);
+		InicioDao.crearPropiedad(InicioDto);
+	}
+	
+	public List<inicioDto> listarPropiedades(){
+		return InicioDao.listarPropiedades();
+	}
 
 }
